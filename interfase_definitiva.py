1a170c6272ec7ab36d826347e12238f4c82b70b7
Arnaldo <arnaldopedroquinones@gmail.com>
2024-10-11 13:12:30 -0300

    generando_distintas_paginas

--- interfase_definitiva.py
+++ interfase_definitiva.py
@@ -7,5 +7,20 @@ st.set_page_config(
     page_title="Mi Aplicación",  # Título de la página que debería aparecer en la pestaña del navegador
     page_icon=r"D:\Users\Arnaldo\Desktop\SISTEMAS\practicas\practicas_apis\FASTAPI\proyecto\galactica.ico"  # Ruta completa al ícono
 )
-st. title("Dale Bocaaaaaaaaa!!!!!!")
-st. title("Aguante Messi!!!!!!")
+# --- PAGE SETUP ---
+about_page = st.Page(
+    "views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    "views/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:",
+)
+project_2_page = st.Page(
+    "views/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:",
+)
