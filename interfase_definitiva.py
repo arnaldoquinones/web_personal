ea04a3c967c6fd5ef85b35005cb11fed177bbce7
Arnaldo <arnaldopedroquinones@gmail.com>
2024-10-11 12:08:01 -0300

    Estructurando_el_codigo.

--- interfase_definitiva.py
+++ interfase_definitiva.py
@@ -1,6 +1,11 @@
+# Importo librerias.
 import streamlit as st
 import fastapi as fastAPI
 
-
-st.set_page_config(page_title="Mi Aplicación",    
-    page_icon=(r"D:\Users\Arnaldo\Desktop\SISTEMAS\practicas\practicas_apis\FASTAPI\proyecto\galactica.ico"))
+# -- PAGE SETUP --
+st.set_page_config(
+    page_title="Mi Aplicación",  # Título de la página que debería aparecer en la pestaña del navegador
+    page_icon=r"D:\Users\Arnaldo\Desktop\SISTEMAS\practicas\practicas_apis\FASTAPI\proyecto\galactica.ico"  # Ruta completa al ícono
+)
+st. title("Dale Bocaaaaaaaaa!!!!!!")
+st. title("Aguante Messi!!!!!!")
