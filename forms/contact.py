fd6907528da472d11263e6c469197c16814c98ee
Arnaldo <arnaldopedroquinones@gmail.com>
2024-10-13 21:13:29 -0300

    pop_up_resuelto

--- forms/contact.py
+++ forms/contact.py
@@ -3,7 +3,7 @@ import streamlit as st
 import requests  # pip install requests
 
 
-WEBHOOK_URL = st.secrets["WEBHOOK_URL"]
+WEBHOOK_URL = "https://hooks.pabbly.com/api/webhook/670c56b88df01fec66f88c3f"
 
 
 def is_valid_email(email):
